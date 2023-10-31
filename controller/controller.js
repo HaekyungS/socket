6c65c20027ee4992864559df29885fa0251ae282
HaekyungSeong <haekyungs95@gmail.com>
2023-10-31 15:35:30 +0900

    소켓서버 연결

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,16 @@
 import express from "express";
 import path from "path";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 // 절대경로 사용을 위한 경로 설정
 const __dirname = path.resolve("..");
 
 const app = express();
+// 소켓연결을 위한 서버
+const server = createServer(app);
+// 소켓서버
+const io = new Server(server);
 
 // 정적 파일 사용을 위한 express.static 경로 설정
 app.use(express.static(path.join(__dirname, "view")));
@@ -25,6 +31,16 @@ app.get("/module/tagMaker.js", (req, res) => {
   res.sendFile("tagMaker.js", { root: path.join(__dirname, "module") });
 });
 
-app.listen(3000, () => {
+// 소켓입장 시
+io.on("connection", (socket) => {
+  console.log("socket server is on");
+  console.dir(socket);
+
+  socket.on("disconnect", () => {
+    console.log("byebye");
+  });
+});
+
+server.listen(3000, () => {
   console.log("port 3000 실행중");
 });
