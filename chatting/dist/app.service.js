cdd83f98675159a2a1b64e40cbc3014efaede943
HaekyungSeong <haekyungs95@gmail.com>
2023-11-21 13:59:36 +0900

    서버 실행 시 멘트 수정

--- chatting/dist/app.service.js
+++ chatting/dist/app.service.js
@@ -10,7 +10,7 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return '김루이 건강 챙겨~~~';
+        return 'NestJS ON';
     }
 };
 exports.AppService = AppService;
