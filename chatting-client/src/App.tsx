7c953e430d6463dda0a196172eb7ddd20f60a899
HaekyungSeong <haekyungs95@gmail.com>
2023-11-21 13:58:30 +0900

    socket 연결까진 됨. 상대 메세지가 전송되는 게 오류나고 반복적인 콘솔메세지 발생

--- chatting-client/src/App.tsx
+++ chatting-client/src/App.tsx
@@ -1,22 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import io from "socket.io-client";
 
 export const App = () => {
   // 입력될 채팅
   const [chatText, setChatText] = useState("");
+  // 내가 쓴 채팅
   const [chatList, setChatList] = useState<string[]>([]);
+  // 남이 쓴 채팅
+  const [otherChat, setOtherChat] = useState<string[]>([]);
+
+  const socket = io("http://localhost:3000", { transports: ["websocket"] });
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("socket connected");
+
+      // 내 채팅을 다른 유저들에게 전달.
+      socket.emit("message", chatText);
+
+      socket.on("message", (data: string) => {
+        setOtherChat([...otherChat, data]);
+      });
+    });
+  }, [chatText, otherChat, socket]);
 
   // text 입력받으면 추가시킬 함수
   const chatInput = (text: string) => {
     return <li>{text}</li>;
   };
 
-  const chatChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChatText(event.target.value);
-    console.log(chatText);
+  const chatOtherInput = (text: string) => {
+    return <li className="otherChat">{text}</li>;
   };
 
+  const chatChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChatText(event.target.value);
+  };
+
+  // 내가 쓴 채팅
   const chatAdd = () => {
+    socket.emit("message", chatText);
+
     setChatList([...chatList, chatText]);
     setChatText("");
   };
@@ -35,7 +60,10 @@ export const App = () => {
         {/* 메인채팅 */}
         <ol className="chatMain">
           {/* 아래 전송 누르면 여기 생겨야함 */}
+          {/* 내 채팅뜨는거고 */}
           {chatList.map(chatInput)}
+          {/* 남의채팅뜨는건데 */}
+          {otherChat.map(chatOtherInput)}
         </ol>
 
         {/* 하단입력부 */}
